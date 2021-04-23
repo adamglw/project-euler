0231ab3ea60e4730ab46ec624ff39d914a259f8a
adamglw <53101365+adamglw@users.noreply.github.com>
2021-04-23 10:18:50 +0100

    Add largest palindrome product in C & minor edits

--- C/2-even-fibonacci-numbers/even-fibonacci-number.c
+++ C/2-even-fibonacci-numbers/even-fibonacci-number.c
@@ -5,7 +5,7 @@
  * 
  * By considering the terms in the Fibonacci sequence whose values do not
  * exceed four million, find the sum of the even-valued terms.
-*/
+ */
 #include<stdio.h>
 
 long fibonacci(int n);
