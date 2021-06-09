0d5f798de4edfc855be366020c4cdf32c232b5fb
adamglw <53101365+adamglw@users.noreply.github.com>
2021-06-09 23:51:56 +0100

    Start prime method for finding smallest multiple

---
+++ C/5-smallest-multiple/smallest-multiple-prime-factors.c
@@ -0,0 +1,16 @@
+/* 2520 is the smallest number that can be divided by each of the numbers from
+ * 1 to 10 without any remainder. 
+ *
+ * What is the smallest positive number that is evenly divisible by all of the
+ * numbers from 1 to 20?
+ */
+
+/* We can find the smallest number divisible by 1 through 20 by finding the
+ * prime factos of the integers 20 and below and multiplying them together.
+ */
+
+#include<stdio.h>
+
+int main(void){
+
+}
